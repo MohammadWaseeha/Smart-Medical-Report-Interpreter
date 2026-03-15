91ed499a6ce75bb7a1b891ccb872cc538a01ce00
MohammadWaseeha <waseehamohammad@gmail.com>
2026-03-15 11:25:05 +0530

    Adding files

--- modules/chatbot.py
+++ modules/chatbot.py
@@ -1,7 +1,7 @@
-import streamlit as st
+from config import GEMINI_API_KEY
 import google.generativeai as genai
 
-genai.configure(api_key=st.secrets["GOOGLE_API_KEY"])
+genai.configure(api_key=GEMINI_API_KEY)
 
 model = genai.GenerativeModel("gemini-2.5-flash")
 
